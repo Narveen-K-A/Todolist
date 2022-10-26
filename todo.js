c7cc2761c9350bf1e2001c49fc11e14e4b884933
Narveen K A <narveen@techversantinfotech.com>
2022-10-26 17:50:43 +0530

    File Commit

--- todo.js
+++ todo.js
@@ -1,11 +1,32 @@
-const newTaskInput = document.querySelector("#newtask input");
-const tasksDiv = document.querySelector("#tasks");
-let deleteTasks, editTasks, tasks;
-let updateNote = "";
-let count;
-
-window.onload = () => {
-	updateNote = "";
-	count = Object.keys(localStorage).length;
-	displayTasks();
-};
+var close = document.getElementsByClassName("close");
+var i;
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev){
+	if(ev.target.tagName === 'LI'){
+		ev.target.classList.toggle('checked');
+	}
+});
+function newElement(){
+	var li = document.createElement("li");
+	var inputValue = document.getElementById("myInput").value;
+	var t = document.createTextNode(inputValue);
+	li.appendChild(t);
+	if(inputValue === ''){
+		alert("Please enter the task!");
+  	} 
+	else{
+		document.getElementById("listed").appendChild(li);
+  	}
+  	document.getElementById("myInput").value = "";
+	var span = document.createElement("span");
+	var txt = document.createTextNode("\u00D7");
+	span.className = "close";
+	span.appendChild(txt);
+	li.appendChild(span);
+	for (i = 0; i < close.length; i++){
+		close[i].onclick = function(){
+			var div = this.parentElement;
+			div.style.display = "none";
+		}
+	}
+}
